bf5817a01361f635506ecec8531d4c893b5af68a
Jeremy Soller <jackpot51@gmail.com>
2022-01-19 14:16:12 -0700

    Update debug info, try setting FCH_PWRGD

--- src/board/system76/sunrise/board.c
+++ src/board/system76/sunrise/board.c
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 #include <board/board.h>
+#include <ec/gpio.h>
 
-void board_init(void) {}
+Gpio FCH_PWRGD = GPIO(0x0D);
+void board_init(void) {
+    //TODO: figure out how the vendor EC firmware handles this
+    gpio_set_output(&FCH_PWRGD, false);
+}
 
 void board_event(void) {}
